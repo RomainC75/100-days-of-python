43ffc2593a22acbd530917516bba38adc49322f2
RomainC75 <rom.chenard@gmail.com>
2022-11-19 18:17:58 +0100

    d45 : done

--- d46-beautifulSoup-billboard/main.py
+++ d46-beautifulSoup-billboard/main.py
@@ -6,6 +6,8 @@ SPOTIPY_CLIENT_ID= config['SPOTIPY_CLIENT_ID']
 SPOTIPY_CLIENT_SECRET = config['SPOTIPY_CLIENT_SECRET']
 SPOTIPY_REDIRECT_URI = config['SPOTIPY_REDIRECT_URI']
 
+scrape = Scrape()
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -21,22 +23,22 @@ sp = spotipy.Spotify(
 )
 user_id = sp.current_user()["id"]
 
-
-scrape = Scrape()
-# create playlist
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 song_names = scrape.get_top_100(date)
 
 song_uris = []
 year = date.split("-")[0]
-for song in song_names[:5]:
+for song in song_names:
     song = song['title']
     result = sp.search(q=f"track:{song} year:{year}", type="track")
-    print(result)
+    # print(result)
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
-print(song_uris)
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date}! Romain' Birthday Billboard", public=False)
+print(playlist)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
